6ea72ac4e3c0d97b40b62944c7df9a18d1ab42a0
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-02-11 01:07:29 +0800

    feat: user specific list of expense

--- controllers/expense-controller.js
+++ controllers/expense-controller.js
@@ -1,89 +1,131 @@
 const Expense = require('../models/expense')
+const Category = require('../models/category')
 const expenseController = {
-	spendingList: (req, res, next) => {
-		Expense.find()
-			.lean()
-			.then(expenses => {
-				let totalSpending=0
-				for (const element of expenses) {
-					if (element.type === '收') {
-						totalSpending += element.cost
-					} else {
-						totalSpending -= element.cost
-					}
+	spendingList: async (req, res, next) => {
+		try {
+			const userId = req.user._id
+			const data = await Expense.find({ userId }).lean()
+			const categories = await Category.find().lean()
+			const expenses = data.map(expense => ({
+				...expense,
+				category: categories.find(
+					element => String(element._id) === String(expense.categoryId)
+				),
+			}))
+			console.log(data)
+			let totalSpending = 0
+			for (const element of expenses) {
+				if (element.type === '收') {
+					totalSpending += element.cost
+				} else {
+					totalSpending -= element.cost
 				}
-				res.render('index', { expenses, totalSpending })
-			})
-			.catch(err => next(err))
+			}
+			
+			res.render('index', { expenses, totalSpending, categories })
+		} catch (err) {
+			return next(err)
+		}
 	},
-	sortingList:(req, res, next) => {
+	sortingList: (req, res, next) => {
 		res.send('it is going to sort data')
 	},
-	getNewExpense: (req, res) => {
-		res.render('new')
-	},
-	getEditExpence: (req, res, next) => {
-		const expenseId = req.params.id
-		Expense.findById(expenseId)
-			.lean()
-			.then(expense => res.render('edit', { expense }))
-			.catch(err => next(err))
-	},
-	postNewExpense: (req, res, next) => {
-		const { type, name, date, category, cost } = req.body
+	getNewExpense: async (req, res, next) => {
 		try {
-			expenseController.checkNewOrEditInput(type, name, date, category, cost)
+			const categories = await Category.find().lean()
+			res.render('new', { categories })
 		} catch (err) {
 			return next(err)
 		}
-		return Expense.create({
-			type,
-			name,
-			date,
-			category,
-			cost,
-		})
-			.then(() => {
-				req.flash('success_messages', 'expense was successfully created')
-				res.redirect('/')
-			})
-			.catch(err => next(err))
 	},
-	putEditExpence: (req, res, next) => {
-		const expenseId = req.params.id
-		const { type, name, date, category, cost } = req.body
+	getEditExpence: async (req, res, next) => {
 		try {
-			expenseController.checkNewOrEditInput(type, name, date, category, cost)
+			
+			const expenseId = req.params.id
+			const expense = await Expense.findById(expenseId).lean()
+			const categories = await Category.find().lean()
+			res.render('edit', {
+				expense,
+				categories,
+				categoryId: expense.categoryId,
+			})
 		} catch (err) {
 			return next(err)
 		}
-		Expense.findById(expenseId)
-			.then( expense => {
-				 for (const key in req.body) {
-				 expense[key] = req.body[key]
-				}
-				return expense.save()
+	},
+	postNewExpense: async (req, res, next) => {
+		try {
+			const userId = req.user._id
+			const { type, name, date, categoryId, cost } = req.body
+			try {
+				expenseController.checkNewOrEditInput(
+					type,
+					name,
+					date,
+					categoryId,
+					cost,
+				)
+				
+			} catch (err) {
+				return next(err)
+			}
+			await Expense.create({
+				type,
+				name,
+				date,
+				categoryId,
+				cost,
+				userId,
 			})
-			.then(() => res.redirect(`/`))
-			.catch(err => next(err))
+			req.flash('success_messages', 'expense was successfully created')
+			res.redirect('/')
+		} catch (err) {
+			return next(err)
+		}
 	},
-	deleteExpence: (req, res, next) => { 
-		const expenseId = req.params.id
-		console.log(req.params)
-		if(!expenseId) throw new Error('不存在的物件或使用者')
-		Expense.findById(expenseId)
-			.then(expense => expense.remove())
-			.then(() => res.redirect(`/`))
-    	.catch((err) => next(err));
-
+	putEditExpence: async (req, res, next) => {
+		try {
+			const expenseId = req.params.id
+			const { type, name, date, categoryId, cost } = req.body
+			try {
+				expenseController.checkNewOrEditInput(
+					type,
+					name,
+					date,
+					categoryId,
+					cost
+				)
+			} catch (err) {
+				return next(err)
+			}
+			const expense = await Expense.findById(expenseId)
+			for (const key in req.body) {
+				expense[key] = req.body[key]
+			}
+			await expense.save()
+			res.redirect(`/`)
+		} catch (err) {
+			return next(err)
+		}
+	},
+	deleteExpence: async (req, res, next) => {
+		try {
+			const expenseId = req.params.id
+			if (!expenseId) throw new Error('不存在的物件或使用者')
+			const expense = await Expense.findById(expenseId)
+			await expense.remove()
+			res.redirect(`/`)
+		} catch (err) {
+			return next(err)
+		}
 	},
 
-	checkNewOrEditInput: function (type, name, date, category, cost) {
+	checkNewOrEditInput: function (type, name, date, categoryId, cost) {
 		if (type.length !== 1 && (type !== '收' || type !== '支'))
 			throw new Error('不存在的收支方法')
 		if (!name) throw new Error('名稱為必填')
 		if (!date) throw new Error('日期為必填')
-		if (!category) throw new Error('請選擇收支總類')
+		if (!String(categoryId)) throw new Error('請選擇收支總類')
 		if (!cost || Number(cost) < 0) throw new Error('請輸入大於0的『數字』')
 	},
 }
