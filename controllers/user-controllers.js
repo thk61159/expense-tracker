dcdb18856b758db616dc63ca74d7c5ca776a228e
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-02-11 16:09:41 +0800

    feat: register controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,4 +1,6 @@
 const passport = require('passport')
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 const userController = {
 	loginPage: (req, res) => {
 		res.render('login')
@@ -12,21 +14,50 @@ const userController = {
 			if (!user) return res.redirect('/login')
 			req.logIn(user, err => {
 				if (err) return next(err)
-
 				return res.redirect('/')
 			})
 		})(req, res, next)
 	},
-	register: (req, res) => {
-		const { name, email, password, confirmPassword } = req.body
-		res.redirect('/login')
+	register: async (req, res, next) => {
+		try {
+			const { name, email, password, confirmPassword } = req.body
+			try {
+				userController.checkRegisterInput(
+					name,
+					email,
+					password,
+					confirmPassword
+				)
+			} catch (err) {
+				console.log(err)
+				return next(err)
+			}
+			const user = await User.find({ email })
+			console.log(user)
+			if (user.length > 0) throw new Error('此信箱已被註冊')
+			const salt = await bcrypt.genSalt(10)
+			const hash = await bcrypt.hash(password, salt)
+			await User.create({
+				name,
+				email,
+				password: hash,
+			})
+			res.redirect('/login')
+		} catch (err) {
+			next(err)
+		}
 	},
 	logout: (req, res) => {
 		req.logout(req.user, err => {
 			if (err) return next(err)
-			req.flash('success_msg', '你已經成功登出。')
+			req.flash('success_messages', '你已經成功登出。')
 			res.redirect('/login')
 		})
+	},
+	checkRegisterInput: function (name, email, password, confirmPassword) {
+		if (!(name && email && password && confirmPassword))
+			throw new Error('所有空格為必填')
+		if (password !== confirmPassword) throw new Error('密碼與確認密碼不符')
 	},
 }
 
