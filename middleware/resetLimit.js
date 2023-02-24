1a7c64145a2a36e8cf884db4bd59927f53f9f4ee
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-02-24 15:41:32 +0800

    fix: remove test code

--- middleware/resetLimit.js
+++ middleware/resetLimit.js
@@ -5,8 +5,6 @@ module.exports = {
 			const { token } = req.params
 			jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
 				if (!decoded) {
-					console.log(decoded)
-					console.log('test')
 					req.flash(
 						'warning_messages',
 						'重設密碼網址失效！請重新發送重設密碼信件'
@@ -16,7 +14,6 @@ module.exports = {
         return next()
 			})
 		} catch (err) {
-			console.log(err,'============================')
 			return next(err)
 		}
 	},
